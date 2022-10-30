11858659d47652c54f279276d3471c3ee068d8fc
xmh <xmh@qq.com>
2022-10-30 19:26:54 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @Description:
+ * @Auther: xumh
+ * @Date: 2022/10/30 19:22
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
